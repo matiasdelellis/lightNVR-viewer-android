b8b1b46216edef33d3c3c7123ba5ee886cf97411
Matias De lellis <mati86dl@gmail.com>
2025-04-02 20:47:22 -0300

    Take snapshot and share image..

--- app/src/main/java/ar/com/delellis/lightnvrviewer/VideoActivity.java
+++ app/src/main/java/ar/com/delellis/lightnvrviewer/VideoActivity.java
@@ -2,14 +2,18 @@ package ar.com.delellis.lightnvrviewer;
 
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
+import static android.widget.Toast.LENGTH_LONG;
 
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
@@ -18,10 +22,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.util.VLCVideoLayout;
 
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 import ar.com.delellis.lightnvrviewer.api.ApiClient;
@@ -29,6 +40,7 @@ import ar.com.delellis.lightnvrviewer.api.ApiService;
 import ar.com.delellis.lightnvrviewer.api.Recording;
 import ar.com.delellis.lightnvrviewer.api.Recordings;
 import ar.com.delellis.lightnvrviewer.api.Stream;
+import ar.com.delellis.lightnvrviewer.api.Util;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -103,6 +115,9 @@ public class VideoActivity extends AppCompatActivity {
         String videoUrl = ApiClient.getInstance().getLiveUrl(streamName);
         Log.i(TAG, "Playing video url: " + videoUrl);
         vlcPlayer.playUri(Uri.parse(videoUrl));
+
+        FloatingActionButton takeSnapshot = findViewById(R.id.takeSnapshot);
+        takeSnapshot.setOnClickListener(v -> takeSnapshot());
     }
 
     @Override
@@ -150,7 +165,7 @@ public class VideoActivity extends AppCompatActivity {
                     String recordingUrl = null;
                     int recordingId = recording.getId();
                     if (recordingId > 0) {
-                        recordingUrl = apiClient.getRecordingUrl(recording.getId());
+                        recordingUrl = apiClient.getRecordingUrl(recordingId);
                     } else {
                         recordingUrl = apiClient.getLiveUrl(recording.getStream());
                     }
@@ -167,6 +182,27 @@ public class VideoActivity extends AppCompatActivity {
             public void onFailure(Call<Recordings> call, Throwable throwable) {
                 // TODO
                 throwable.printStackTrace();
+            }
+        });
+    }
+    private void takeSnapshot() {
+        SurfaceView surfaceView = findViewById(org.videolan.R.id.surface_video);
+        Util.getSurfaceBitmap(surfaceView, new Util.PixelCopyListener() {
+            @Override
+            public void onSurfaceBitmapReady(Bitmap bitmap) {
+                File snapshotFile = Util.getSnapshotFile(currentStream.getName());
+                try {
+                    OutputStream out = new BufferedOutputStream(new FileOutputStream(snapshotFile));
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 0, out);
+                    Util.shareImage(VideoActivity.this, Uri.parse(snapshotFile.getPath()), currentStream.getName());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+            @Override
+            public void onSurfaceBitmapError(String errorMsg) {
+                Toast.makeText(VideoActivity.this, errorMsg, LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/ar/com/delellis/lightnvrviewer/api/Util.java
+++ app/src/main/java/ar/com/delellis/lightnvrviewer/api/Util.java
@@ -1,8 +1,20 @@
 package ar.com.delellis.lightnvrviewer.api;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.PixelCopy;
+import android.view.SurfaceView;
+
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class Util {
@@ -22,5 +34,48 @@ public class Util {
         }
 
         return dueDateAsNormal;
+    }
+
+    public static File getSnapshotFile(String streamName) {
+        File snapshotFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Screenshots");
+        snapshotFolder.mkdirs();
+
+        String currentDate = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String fileName = "lightNVR_" + streamName + "_" + currentDate + ".png";
+
+        return new File(snapshotFolder.getPath() + "/" + fileName);
+    }
+
+    public static void getSurfaceBitmap(SurfaceView surfaceView, final PixelCopyListener listener) {
+        Bitmap bitmap = Bitmap.createBitmap(surfaceView.getWidth(), surfaceView.getHeight(), Bitmap.Config.ARGB_8888);
+
+        HandlerThread handlerThread = new HandlerThread(Util.class.getSimpleName());
+        handlerThread.start();
+
+        PixelCopy.request(surfaceView, bitmap, new PixelCopy.OnPixelCopyFinishedListener() {
+            @Override
+            public void onPixelCopyFinished(int copyResult) {
+                if (copyResult == PixelCopy.SUCCESS) {
+                    listener.onSurfaceBitmapReady(bitmap);
+                } else {
+                    listener.onSurfaceBitmapError("Couldn't create bitmap of the SurfaceView: " + String.valueOf(copyResult));
+                }
+                handlerThread.quitSafely();
+            }
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+    public static void shareImage(Context context, Uri uri, String title) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        shareIntent.setType("image/png");
+        context.startActivity(Intent.createChooser(shareIntent, title));
+    }
+
+    public interface PixelCopyListener {
+        void onSurfaceBitmapReady(Bitmap bitmap);
+
+        void onSurfaceBitmapError(String errorMsg);
     }
 }
